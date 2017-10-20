e44e3e06a6fa927b9ded1a9f5076c3c7d7fa09ef
Fatih AKSOY <fatihaksoy59@hotmail.com>
2017-10-21 02:52:29 +0300

    Mp3 Player

---
+++ FatihPlayer/app/src/main/java/com/example/a/fatihplayer/MainActivity.java
@@ -0,0 +1,232 @@
+package com.example.a.fatihplayer;
+
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Environment;
+import android.os.Handler;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView startTimeTV,endTimeTV;
+    private MediaPlayer mediaPlayer;
+    private ImageButton playBtn;
+    private ImageButton nextBtn;
+    private ImageButton prevBtn;
+    private SeekBar songSB;
+    private ListView songsLV;
+
+    int myPosition=0;
+    int mySongsSize=0;
+    private double currentTime=0;
+    private double durationTime=0;
+    ArrayList<String> mySongsName;
+    ArrayList<String> mySongsLoc;
+
+    private Handler myHandler=new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        playBtn =(ImageButton)findViewById(R.id.playBtn);
+        nextBtn =(ImageButton)findViewById(R.id.nextBtn);
+        prevBtn =(ImageButton)findViewById(R.id.prevBtn);
+        startTimeTV   =(TextView)findViewById(R.id.startTimeTV);
+        endTimeTV =(TextView)findViewById(R.id.endTimeTV);
+        songSB=(SeekBar)findViewById(R.id.songSB);
+        songsLV=(ListView)findViewById(R.id.songLV);
+
+        mySongsName=new ArrayList<>();
+        mySongsLoc=new ArrayList<>();
+
+
+        /*
+        *
+        * Çalma Listesi Oluştur Array List ile
+        *
+        * */
+
+        getMusic();
+
+        ArrayAdapter<String> adp=new ArrayAdapter<>(this,R.layout.songs_layout,R.id.textView,mySongsName);
+        songsLV.setAdapter(adp);
+
+        Uri uri =Uri.parse(mySongsLoc.get(myPosition).toString());
+        prevBtn.setClickable(false);
+        mediaPlayer=MediaPlayer.create(getApplicationContext(),uri);
+
+        songsLV.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                myPosition=position;
+                buttonKontrol();
+                play();
+            }
+        });
+
+        songSB.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                mediaPlayer.seekTo(songSB.getProgress());
+            }
+        });
+
+    }
+
+
+    public void setTimes(){
+        durationTime=mediaPlayer.getDuration();
+        currentTime=mediaPlayer.getCurrentPosition();
+
+        songSB.setMax((int) durationTime);
+        endTimeTV.setText(String.format("%d min, %d sec",
+                TimeUnit.MILLISECONDS.toMinutes((long) durationTime),
+                TimeUnit.MILLISECONDS.toSeconds((long) durationTime) -
+                        TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.
+                                toMinutes((long) durationTime)))
+        );
+
+        startTimeTV.setText(String.format("%d min, %d sec",
+                TimeUnit.MILLISECONDS.toMinutes((long) currentTime),
+                TimeUnit.MILLISECONDS.toSeconds((long) currentTime) -
+                        TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.
+                                toMinutes((long) currentTime)))
+        );
+
+        songSB.setProgress((int)currentTime);
+        myHandler.postDelayed(UpdateSongTime,100);
+    }
+
+    public void clickBtn(View view){
+
+        try {
+            if (!mediaPlayer.isPlaying()) {
+
+
+                mediaPlayer.start();
+                playBtn.setImageResource(R.drawable.pause);
+
+                setTimes();
+
+
+            } else {
+                mediaPlayer.pause();
+
+                playBtn.setImageResource(R.drawable.play);
+            }
+        }catch (Exception e){
+            Log.e("hata",e.getMessage());
+        }
+
+
+
+    }
+
+    public void play(){
+        if(mediaPlayer.isPlaying()){
+            mediaPlayer.stop();
+            Uri uri2 =Uri.parse(mySongsLoc.get(myPosition).toString());
+            mediaPlayer=MediaPlayer.create(getApplicationContext(),uri2);
+            mediaPlayer.start();
+            setTimes();
+        }else{
+
+            Uri uri2 =Uri.parse(mySongsLoc.get(myPosition).toString());
+            mediaPlayer=MediaPlayer.create(getApplicationContext(),uri2);
+            setTimes();
+        }
+    }
+
+    public void buttonKontrol(){
+        if(mySongsSize-1==myPosition){
+            nextBtn.setClickable(false);
+        }else{
+            nextBtn.setClickable(true);
+        }
+        if(myPosition<=0){
+            prevBtn.setClickable(false);
+        }else{
+            prevBtn.setClickable(true);
+        }
+    }
+
+    public void prevBtnClick(View view){
+        myPosition--;
+        buttonKontrol();
+        play();
+    }
+
+    public void nextBtnClick(View view){
+        myPosition++;
+        buttonKontrol();
+        play();
+    }
+
+    public void getMusic (){
+        ContentResolver contentResolver =getContentResolver();
+        Uri songUri= MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        Cursor songCursor=contentResolver.query(songUri,null,null,null,null);
+
+        if(songCursor!=null &&songCursor.moveToFirst()){
+            int songTitle=songCursor.getColumnIndex(MediaStore.Audio.Media.TITLE);
+            int songLocation=songCursor.getColumnIndex(MediaStore.Audio.Media.DATA);
+            do{
+                int i=0;
+                String songLoc = songCursor.getString(songLocation);
+                String songTit = songCursor.getString(songTitle);
+                mySongsName.add(songTit);
+                mySongsLoc.add(songLoc);
+            }while (songCursor.moveToNext());
+        }
+        mySongsSize=mySongsName.size();
+    }
+
+    private Runnable UpdateSongTime = new Runnable() {
+        public void run() {
+            if(!mediaPlayer.isPlaying()){
+
+                playBtn.setImageResource(R.drawable.play);
+            }
+            currentTime = mediaPlayer.getCurrentPosition();
+            startTimeTV.setText(String.format("%d min, %d sec",
+                    TimeUnit.MILLISECONDS.toMinutes((long) currentTime),
+                    TimeUnit.MILLISECONDS.toSeconds((long) currentTime) -
+                            TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.
+                                    toMinutes((long) currentTime)))
+            );
+
+            songSB.setProgress((int)currentTime);
+            myHandler.postDelayed(this, 100);
+        }
+    };
+}
